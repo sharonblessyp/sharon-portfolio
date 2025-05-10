e0893f21e10d22faa1f392258dd169769e78c650
PSharonBlessy15 <sharonblessyp@gmail.com>
2025-05-10 22:25:42 +0530

    moved file to public

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/sharon-portfolio/',
+  base: '/sharon-portfolio/',
   plugins: [react()],
 })
